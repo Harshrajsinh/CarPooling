797ee01031303bba606d0ddfb1409680bac54443
vijay <intern.vijayj@rishabhsoft.com>
2016-02-26 03:45:43 -0500

    login screen

--- CBE/app/src/main/java/com/cbe/android/ui/login/CBELogin.java
+++ CBE/app/src/main/java/com/cbe/android/ui/login/CBELogin.java
@@ -15,8 +15,9 @@ public class CBELogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cbelogin);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
 
 
     }
