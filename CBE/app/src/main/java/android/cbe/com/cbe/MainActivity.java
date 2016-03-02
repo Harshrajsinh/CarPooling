72e82cd755bf6908f035471b3d24bb15080b3ac9
vijay <intern.vijayj@rishabhsoft.com>
2016-03-02 01:21:46 -0500

    no changes..

--- CBE/app/src/main/java/android/cbe/com/cbe/MainActivity.java
+++ CBE/app/src/main/java/android/cbe/com/cbe/MainActivity.java
@@ -14,18 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.content_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.common_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+
     }
 
     @Override
--- CBE/app/src/main/java/android/cbe/com/cbe/baseapp/baseclass/BaseActivity.java
+++ CBE/app/src/main/java/android/cbe/com/cbe/baseapp/baseclass/BaseActivity.java
@@ -1,6 +1,44 @@
 package android.cbe.com.cbe.baseapp.baseclass;
-/**
- * Created by Intern.harshrajT on 2/23/2016.
- */
+
+import android.cbe.com.cbe.R;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
+
+
 public class BaseActivity {
+
+  /*  private Toolbar mToolbar;
+
+ss
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.activity_main);
+
+        //Set custom Toolbar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(mToolbar);
+
+    }
+
+
+    public void setToolbar(AppCompatActivity context, Toolbar toolBar, boolean isSubActivity) {
+
+
+        context.getSupportActionBar().setDisplayShowTitleEnabled(false);
+        context.getSupportActionBar().setHomeButtonEnabled(true);
+        if (isSubActivity) {
+            context.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } else {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+
+*/
+
+
 }
