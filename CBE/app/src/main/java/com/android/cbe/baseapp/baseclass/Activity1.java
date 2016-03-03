68a6fe19a0f5792da1244e634c34f174a994b758
vijay <intern.vijayj@rishabhsoft.com>
2016-03-03 04:59:11 -0500

    chnages in baseactivity

--- CBE/app/src/main/java/com/android/cbe/baseapp/baseclass/Activity1.java
+++ CBE/app/src/main/java/com/android/cbe/baseapp/baseclass/Activity1.java
@@ -1,29 +1,48 @@
-package com.android.cbe;
+package com.android.cbe.baseapp.baseclass;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.android.cbe.baseapp.baseclass.BaseActivity;
 import com.cbe.android.R;
 
-public class MainActivity extends BaseActivity {
+public class Activity1 extends BaseActivity {
+
+
+    private View rootView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.activity_1);
+        rootView = findViewById(R.id.activity1_container);
+
+        /* use rootView to get UI content from main container */
+        //BT bt = (BT) rootView.findViewById(R.id.fab) ...;
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Activity1", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        //Set nav drawer selected to first item in list
+        mNavigationView.getMenu().getItem(0).setChecked(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_activity1, menu);
         return true;
     }
 
--- CBE/app/src/main/java/com/android/cbe/baseapp/baseclass/BaseFragment.java
+++ CBE/app/src/main/java/com/android/cbe/baseapp/baseclass/BaseFragment.java
@@ -1,9 +1,46 @@
 package com.android.cbe.baseapp.baseclass;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.cbe.android.R;
 
 /**
  * Created by Intern.harshrajT on 2/23/2016.
  */
-public class BaseFragment {
+public class BaseFragment extends Fragment {
+
+   /* protected FrameLayout baseFrameLayout;
+    protected View baseView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        baseView = inflater.inflate(R.layout.fragment_base_layout, container, false);
+
+        baseFrameLayout = (FrameLayout) baseView.findViewById(R.id.frameLayout);
+
+        return baseView;
+    }*/
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(
+                R.layout.fragment_base_layout, container, false);
+    }
 }
